e0a55eaae2f690f327a58676c8fa2215f3632d9d
SOURADIP-DAS-ai <souradipdas50@gmail.com>
2024-06-05 19:51:20 +0000

    complete

---
+++ basicjs/03conversionoperation.js
@@ -0,0 +1,41 @@
+/*let score=33
+console.log(typeof(score))
+let score1="33"
+console.log(typeof(score1))
+let value=Number(score1)//conversion string to number
+console.log(typeof value);*/
+
+// if 
+/*let score="33abc"
+let value=Number(score)
+console.log(typeof value)
+console.log(value)*/ //Give the output NaN =>so js convert into any string into number but when we print this then it gives NaN,This is the drawback of Number in js
+
+/*let score=null
+let value=Number(score)
+console.log(typeof value)
+console.log(value) //Give the output 0=>so js convert into null into number but when we print this then it gives 0,This is the drawback of Number in js
+//Number(true)=>1 and Number(false)=>0 and NaN=>Not an Number*/
+
+/*let a=1
+let b=Boolean(a)
+console.log(typeof b)
+console.log(b) //output=>true*/
+
+/*let a=""
+let b=Boolean(a)
+console.log(typeof b)
+console.log(b) *///output=>false
+
+/*let a="hitesh"
+let b=Boolean(a)
+console.log(typeof b)
+console.log(b)*/ //output=>true
+
+let a=33
+let b=String(33)
+console.log(typeof b) //=>String
+
+
+
+
