b1763ea50fecb31efc14cc2efdabb87e146d1962
SOURADIP-DAS-ai <souradipdas50@gmail.com>
2024-06-05 19:49:31 +0000

    complete

---
+++ basicjs/01variable.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="soura@gmail.com"
+var accountPasword="12345"
+console.table([accountId,accountEmail,accountPasword])
+/*
+const:-
+Never be change after declare once.
+var:-
+prefer not use var because of issue in block scope and functional scope
+let:-
+changable after declare once
+*/ 
---
+++ basicjs/02datatype.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as newer version
+//alert("hello") //Give error in node but give a popup in browser
+//Primitive DataType
+//number,bigInt
+//String=>" "
+//Boolean=>true/false
+//null=>standalone value
+//undefined=>value is not defined
+//symbol=>unique
+
+//Non-Primitive Data type or object
+
+
+//console.log(typeof(null))//Give output object,null is a type of object
+//console.log(typeof(undefined))//Give output undefined,null is a type of undefined
